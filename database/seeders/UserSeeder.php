b2ec912620c79763c6b3ac351746b8cb05eebbd0
Rudolph919 <47603362+Rudolph919@users.noreply.github.com>
2023-10-13 07:30:47 +0200

    User seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'first_name' => 'Rudolph',
+                'last_name' => 'Scheepers-White',
+                'email' => 'white.rudi@gmail.com',
+                'phone_number' => '0796911583',
+                'password' => bcrypt('secret99'),
+            ],
+            [
+                'first_name' => 'Elmereth',
+                'last_name' => 'Scheepers-White',
+                'email' => 'elmereths@gmail.com',
+                'phone_number' => '0825783159',
+                'password' => bcrypt('secret99'),
+            ],
+
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
