6c161f66d44cec6959845babd9b09e618398519b
Rudolph919 <47603362+Rudolph919@users.noreply.github.com>
2023-10-14 04:12:03 +0200

    Broken components

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+defineProps({
+    optionList: {
+        type: Object
+    }
+});
+
+</script>
+
+<template>
+    <select class="mt-1 border border-gray-300 text-gray-600 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
+        <option  selected value="">Select a resource type</option>
+        <option v-for="option in optionList" :value="option.id">
+            {{ option.name }}
+        </option>
+
+    </select>
+</template>
+
+
