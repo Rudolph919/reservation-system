b3e2a793d0f29a8b9aaae59579d8932f09b2074a
Rudolph919 <47603362+Rudolph919@users.noreply.github.com>
2023-11-20 10:32:27 +0200

    Format changes

--- resources/js/Layouts/UnauthenticatedLayout.vue
+++ resources/js/Layouts/UnauthenticatedLayout.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -13,24 +13,36 @@ const showingNavigationDropdown = ref(false);
 <template>
     <div>
         <div class="min-h-screen bg-gray-100">
-            <nav class="bg-emerald-500 shadow">
+            <nav class="shadow bg-emerald-500">
                 <div class="container flex items-center justify-between h-52">
                     <div class="flex items-center shrink-0">
                         <Link :href="route('home')">
                             <ApplicationLogo
-                                class="block h-52 w-auto fill-current text-gray-800 pl-6"
+                                class="block w-auto pl-6 text-gray-800 fill-current h-52"
                             />
                         </Link>
                     </div>
 
                     <div class="flex items-center">
-                        <NavLink :href="route('home')" :active="route().current('home')" class="m-4 italic text-xl text-white">
+                        <NavLink
+                            :href="route('home')"
+                            :active="route().current('home')"
+                            class="m-4 text-xl italic text-white"
+                        >
                             Home
                         </NavLink>
-                        <NavLink :href="route('about')" :active="route().current('about')" class="m-4 italic text-xl text-white text-white">
+                        <NavLink
+                            :href="route('about')"
+                            :active="route().current('about')"
+                            class="m-4 text-xl italic text-white"
+                        >
                             About Us
                         </NavLink>
-                        <NavLink :href="route('contact')" :active="route().current('contact')" class="m-4 italic text-xl text-white">
+                        <NavLink
+                            :href="route('contact')"
+                            :active="route().current('contact')"
+                            class="m-4 text-xl italic text-white"
+                        >
                             Contact Us
                         </NavLink>
                     </div>
@@ -38,10 +50,18 @@ const showingNavigationDropdown = ref(false);
                     <div class="flex items-center pr-6">
                         <ul class="flex items-center">
                             <li>
-                                <a href="/login" class="text-gray-600 hover:text-gray-800">Login</a>
+                                <a
+                                    href="/login"
+                                    class="text-gray-600 hover:text-gray-800"
+                                    >Login</a
+                                >
                             </li>
                             <li class="ml-4">
-                                <a href="/register" class="text-gray-600 hover:text-gray-800">Register</a>
+                                <a
+                                    href="/register"
+                                    class="text-gray-600 hover:text-gray-800"
+                                    >Register</a
+                                >
                             </li>
                         </ul>
                     </div>
@@ -50,7 +70,7 @@ const showingNavigationDropdown = ref(false);
 
             <!-- Page Heading -->
             <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
             </header>
